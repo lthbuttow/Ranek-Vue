d35ae282edba78f15233452d6dcc9393af597526
lucasbuttow <lucas_buttow@outlook.com>
2019-11-30 20:53:15 -0300

    criação da busca e estilização dos produtos

--- src/components/ProdutosBuscar.vue
+++ src/components/ProdutosBuscar.vue
@@ -1,12 +1,28 @@
 <template>
-  <input type="text" />
+  <form>
+    <input name="busca" id="busca" type="text" v-model="busca" />
+    <input
+      type="submit"
+      id="lupa"
+      value="Buscar"
+      @click.prevent="buscarProdutos"
+    />
+  </form>
 </template>
 
 <script>
 export default {
-  name: "ProdutosBuscar"
+  data() {
+    return {
+      busca: ""
+    };
+  },
+  methods: {
+    buscarProdutos() {
+      this.$router.push({ query: { q: this.busca } });
+    }
+  }
 };
 </script>
 
-<style>
-</style>
+<style></style>
